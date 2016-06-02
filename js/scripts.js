35c9c8302ea65272476be04e86dc0dec7b7cc833
Benjamin Heflin and Julia Dickey <julia@>
2016-06-02 11:31:43 -0700

    addded pictures to each div

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 $(document).ready (function(){
 
-$("form#creature").change(function(event) {
-  var creature = parseInt($("#creature").val());
-    event.preventDefault();
-    $("#lizards").hide();
-    $("#sharks").hide();
-    $("#narwhals").hide();
-    $("#adorables").hide();
-    if (creature === 1) {
-      $("#lizards").show();
-    }
-    else if (creature ===2)
-      $("#sharks").show();
-    else if (creature ===3)
-      $("#narwhals").show();
-    else  
-      $("#adorables").show();
-
-    }
+  $("select#creature").change(function(event) {
+    var creature = parseInt($("#creature").val());
+      event.preventDefault();
+      $("#lizards").hide();
+      $("#sharks").hide();
+      $("#narwhals").hide();
+      $("#adorables").hide();
+      if (creature === 1) {
+        $("#lizards").show();
+      }
+      else if (creature ===2){
+        $("#sharks").show();
+      } else if (creature ===3){
+        $("#narwhals").show();
+      } else {
+        $("#adorables").show();
+      }
   });
 });
